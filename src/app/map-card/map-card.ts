28da4548a8289335f483c1677ce026c031b12fd1
Essra <ouniessra01@gmail.com>
2026-01-09 16:02:19 +0100

    Final version: fixed map responsiveness, aligned route/map cards, cleaned styleUrls

--- src/app/map-card/map-card.ts
+++ src/app/map-card/map-card.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, Inject, PLATFORM_ID, Input } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, Input, HostListener } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import * as L from 'leaflet';
 
@@ -11,6 +11,8 @@ export class MapCard implements AfterViewInit {
   @Input() fromAddress!: string;
   @Input() toAddress!: string;
 
+  private map!: L.Map;
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit(): void {
@@ -19,40 +21,23 @@ export class MapCard implements AfterViewInit {
     const mapElement = document.getElementById('leaflet-map');
     if (!mapElement) return;
 
-    const map = L.map(mapElement).setView([29.7373, -95.4339], 6); 
+    this.map = L.map(mapElement).setView([29.7373, -95.4339], 6);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+    }).addTo(this.map);
 
-    
-    const startIcon = L.icon({
-      iconUrl: 'assets/images/start.png',
-      iconSize: [40, 40],
-      iconAnchor: [20, 40]
-    });
 
-    const arrivalIcon = L.icon({
-      iconUrl: 'assets/icons/arraival.svg', 
-      iconSize: [30, 30],
-      iconAnchor: [10, 10]
-    });
+   
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 0);
+  }
 
-    
-    const startCoords: [number, number] = [29.7373, -95.4339];  
-    const endCoords: [number, number] = [32.8675, -96.8011];    
-
-    
-    L.marker(startCoords, { icon: startIcon }).addTo(map).bindPopup(`Start: ${this.fromAddress}`);
-    L.marker(endCoords, { icon: arrivalIcon }).addTo(map).bindPopup(`Arrival: ${this.toAddress}`);
-
-    
-    const routeLine = L.polyline([startCoords, endCoords], {
-      color: 'yellow',
-      weight: 4,
-      opacity: 0.8
-    }).addTo(map);
-
-    map.fitBounds(routeLine.getBounds());
+  @HostListener('window:resize')
+  onResize() {
+    if (this.map) {
+      this.map.invalidateSize();
+    }
   }
 }
