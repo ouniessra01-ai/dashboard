0f50328be7a867a9cf1d6d6c4ba92e298bbe6339
Essra Ouni <ouniessra01@gmail.com>
2026-01-12 09:13:12 +0100

    Updated road tracking dashboard with new code changes

--- src/app/map-card/map-card.ts
+++ src/app/map-card/map-card.ts
@@ -24,11 +24,33 @@ export class MapCard implements AfterViewInit {
     this.map = L.map(mapElement).setView([29.7373, -95.4339], 6);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
+      attribution: '© OpenStreetMap contributeurs'
     }).addTo(this.map);
 
+    const fromCoords: [number, number] = [29.7604, -95.3698];
+    const toCoords: [number, number] = [30.2672, -97.7431];
 
-   
+    const startIcon = L.icon({
+      iconUrl: 'assets/images/start.png',
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+      popupAnchor: [0, -40]
+    });
+
+    const arrivalIcon = L.icon({
+      iconUrl: 'assets/icons/arraival.svg',
+      iconSize: [30, 30],
+      iconAnchor: [20, 40],
+      popupAnchor: [0, -40]
+    });
+
+    L.marker(fromCoords, { icon: startIcon }).addTo(this.map).bindPopup("Point de départ : 2972 Westheimer");
+    L.marker(toCoords, { icon: arrivalIcon }).addTo(this.map).bindPopup("Point d’arrivée : 8502 Preston");
+
+    const routeLine = L.polyline([fromCoords, toCoords], { color: '#FFF9C4', weight: 4 }).addTo(this.map);
+
+    this.map.fitBounds(routeLine.getBounds());
+
     setTimeout(() => {
       this.map.invalidateSize();
     }, 0);
